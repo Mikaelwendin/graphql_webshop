ccc0128ed820e04c1312f8869b2e4d881328676a
Mikaelwendin <mikael.wendin83@gmail.com>
2023-01-29 15:48:45 +0100

    Fixade lite mer saker

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -9,308 +9,202 @@ const {
 const { GraphQLError, printType, isNonNullType } = require("graphql");
 const crypto = require("crypto");
 const axios = require("axios").default;
-
-// Create a variable holding the file path (from computer root directory) to the project fiel directory
-const projectDirectory = path.join(__dirname, "..", "data", "projects");
+const productDirectory = path.join(__dirname, "..", "data", "projects");
 const cartDirectory = path.join(__dirname, "..", "data", "carts");
 
 exports.resolvers = {
   Query: {
     getProductById: async (_, args) => {
-      // Place the projectId the user sent in a variable called "projectId"
       const projectId = args.productId;
-      // Create a variable holding the file path (from computer root directory) to the project
-      // file we are looking for
-      const projectFilePath = path.join(projectDirectory, `${projectId}.json`);
-
-      // Check if the requested project actually exists
-      const projectExists = await fileExists(projectFilePath);
-      // If project does not exist return an error notifying the user of this
-      if (!projectExists)
+      const productFilePath = path.join(productDirectory, `${projectId}.json`);
+      const projectExists = await fileExists(productFilePath);
+      if (!projectExists) {
         return new GraphQLError("That project does not exist");
-
-      // Read the project file; data will be returned as a JSON string
-      const projectData = await fsPromises.readFile(projectFilePath, {
+      }
+      const productData = await fsPromises.readFile(productFilePath, {
         encoding: "utf-8",
       });
-      // Parse the returned JSON project data into a JS object
-      const data = JSON.parse(projectData);
-      // Return the data
+      const data = JSON.parse(productData);
       return data;
     },
     getAllProducts: async (_, args) => {
-      // Get an array of file names that exist in the projects directory
-      // (aka a list of all the projects we have)
-      const projects = await getDirectoryFileNames(projectDirectory);
-
-      // Create a variable with an empty array to hold our project data
-      const projectData = [];
-
-      // For each file found in projects...
-      for (const file of projects) {
-        // ... create the filepath for that specific file
-        const filePath = path.join(projectDirectory, file);
-        // Read the contents of the file; will return a JSON string of the project data
+      const products = await getDirectoryFileNames(productDirectory);
+      const productData = [];
+      for (const file of products) {
+        const filePath = path.join(productDirectory, file);
         const fileContents = await fsPromises.readFile(filePath, {
           encoding: "utf-8",
         });
-        // Parse the JSON data from the previous step
         const data = JSON.parse(fileContents);
-        // Push the parsed data to the projectData array
-        projectData.push(data);
+        productData.push(data);
       }
 
-      /* const promises = []
-			projects.forEach((fileName) => {
-				const filePath = path.join(projectDirectory, fileName)
-				promises.push(readJsonFile(filePath))
-			})
-
-			const projectData = await Promise.all(promises) */
-
-      // Return the projectData array (which should now hold the data for all our projects)
-      return projectData;
+      return productData;
     },
     getShoppingCart: async (_, args) => {
-      // Place the projectId the user sent in a variable called "projectId"
-      const projectId = args.shoppingCartId;
-      // Create a variable holding the file path (from computer root directory) to the project
-      // file we are looking for
-      const projectFilePath = path.join(cartDirectory, `${projectId}.json`);
-
-      // Check if the requested project actually exists
-      const projectExists = await fileExists(projectFilePath);
-      // If project does not exist return an error notifying the user of this
-      if (!projectExists)
+      const cartId = args.shoppingCartId;
+      const productFilePath = path.join(cartDirectory, `${cartId}.json`);
+      const productExists = await fileExists(productFilePath);
+      if (!productExists) {
         return new GraphQLError("That project does not exist");
-
-      // Read the project file; data will be returned as a JSON string
-      const projectData = await fsPromises.readFile(projectFilePath, {
+      }
+      const productData = await fsPromises.readFile(productFilePath, {
         encoding: "utf-8",
       });
-      // Parse the returned JSON project data into a JS object
-      const data = JSON.parse(projectData);
-      // Return the data
+      const data = JSON.parse(productData);
       return data;
     },
   },
   Mutation: {
     createProduct: async (_, args) => {
-      // Verify name: om strängen är tom, return:a en error
       if (args.name.length === 0)
         return new GraphQLError("Name must be at least 1 character long");
-
-      // Skapa ett unikt id + data objektet
-      const newProject = {
-        // Generera ett random id (av typ UUID)
+      const newProduct = {
         id: crypto.randomUUID(),
         name: args.name,
         price: args.price,
         description: args.description || "",
-		image: args.image,
+        image: args.image,
       };
-
-      // Skapa filePath för där vi ska skapa våran fil
-      let filePath = path.join(projectDirectory, `${newProject.id}.json`);
-
-      // Kolla att vårat auto-genererade projektid inte har använts förut
+      let filePath = path.join(productDirectory, `${newProduct.id}.json`);
       let idExists = true;
       while (idExists) {
-        const exists = await fileExists(filePath); // kolla om filen existerar
-        console.log(exists, newProject.id);
-        // om filen redan existerar generera ett nytt projektId och uppdatera filePath
+        const exists = await fileExists(filePath);
+        console.log(exists, newProduct.id);
         if (exists) {
-          newProject.id = crypto.randomUUID();
-          filePath = path.join(projectDirectory, `${newProject.id}.json`);
+          newProduct.id = crypto.randomUUID();
+          filePath = path.join(productDirectory, `${newProduct.id}.json`);
         }
-        // uppdatera idExists (för att undvika infinite loops)
         idExists = exists;
       }
-
-      // Skapa en fil för projektet i /data/projects
-      await fsPromises.writeFile(filePath, JSON.stringify(newProject));
-
-      // Return:a våran respons; vårat nyskapade projekt
-      return newProject;
+      await fsPromises.writeFile(filePath, JSON.stringify(newProduct));
+      return newProduct;
     },
     createCart: async (_, args) => {
-      // Verify name: om strängen är tom, return:a en error
-      // ------- if (args.name.length === 0) return new GraphQLError('Name must be at least 1 character long')
-
-      // Skapa ett unikt id + data objektet
-      const newProject = {
-        // Generera ett random id (av typ UUID)
+      const newCart = {
         id: crypto.randomUUID(),
-        total: args.total,
-        products: [{}],
+        total: args.input.total || 0,
+        products: [],
       };
-
-      // Skapa filePath för där vi ska skapa våran fil
-      let filePath = path.join(cartDirectory, `${newProject.id}.json`);
-
-      // Kolla att vårat auto-genererade projektid inte har använts förut
+      let filePath = path.join(cartDirectory, `${newCart.id}.json`);
       let idExists = true;
       while (idExists) {
-        const exists = await fileExists(filePath); // kolla om filen existerar
-        console.log(exists, newProject.id);
-        // om filen redan existerar generera ett nytt projektId och uppdatera filePath
+        const exists = await fileExists(filePath);
+        console.log(exists, newCart.id);
         if (exists) {
-          newProject.id = crypto.randomUUID();
-          filePath = path.join(cartDirectory, `${newProject.id}.json`);
+          newCart.id = crypto.randomUUID();
+          filePath = path.join(cartDirectory, `${newCart.id}.json`);
         }
-        // uppdatera idExists (för att undvika infinite loops)
         idExists = exists;
       }
-
-      // Skapa en fil för projektet i /data/projects
-      await fsPromises.writeFile(filePath, JSON.stringify(newProject));
-
-      // Return:a våran respons; vårat nyskapade projekt
-      return newProject;
+      await fsPromises.writeFile(filePath, JSON.stringify(newCart));
+      return newCart;
     },
     updateProduct: async (_, args) => {
-      // Hämta alla parametrar från args
-      /* const projectId = args.id
-			const projectName = args.name
-			const projectDescription = args.description */
-
-      const { id, name, description, price } = args;
-
-      // Skapa våran filePath
-      const filePath = path.join(projectDirectory, `${id}.json`);
-
-      // Finns det projekt som de vill ändra?
-      // IF (no) return Not Found Error
-      const projectExists = await fileExists(filePath);
-      if (!projectExists)
-        return new GraphQLError("That project does not exist");
-
-      // Skapa updatedProject objekt
-      const updatedProject = {
+      const { id, name, description, price, image } = args.input;
+      const filePath = path.join(productDirectory, `${id}.json`);
+      const productExists = await fileExists(filePath);
+      if (!productExists) {
+        return new GraphQLError("That product does not exist");
+      }
+      const updatedProduct = {
         id,
         name,
         description,
         price,
+        image,
       };
-
-      // Skriv över den gamla filen med nya infon
-      await fsPromises.writeFile(filePath, JSON.stringify(updatedProject));
-
-      // return updatedProject
-      return updatedProject;
+      await fsPromises.writeFile(filePath, JSON.stringify(updatedProduct));
+      return updatedProduct;
     },
     addToCart: async (_, args) => {
       const prodId = args.productId;
       const cartId = args.cartId;
-      // Create a variable holding the file path (from computer root directory) to the project
-      // file we are looking for
       const cartFilePath = path.join(cartDirectory, `${cartId}.json`);
-      const projectFilePath = path.join(projectDirectory, `${prodId}.json`);
+      const productFilePath = path.join(productDirectory, `${prodId}.json`);
       const cartExists = await fileExists(cartFilePath);
-      if (!cartExists) return new GraphQLError("That cart does not exist");
-
-      // Check if the requested project actually exists
-      const projectExists = await fileExists(projectFilePath);
-      // If project does not exist return an error notifying the user of this
-      if (!projectExists)
+      if (!cartExists) {
+        return new GraphQLError("That cart does not exist");
+      }
+      const productExists = await fileExists(productFilePath);
+      if (!productExists) {
         return new GraphQLError("That project does not exist");
-
-      // Read the project file; data will be returned as a JSON string
-      const projectData = JSON.parse(
-        await fsPromises.readFile(projectFilePath, { encoding: "utf-8" })
+      }
+      const productData = JSON.parse(
+        await fsPromises.readFile(productFilePath, { encoding: "utf-8" })
       );
       let cartData = JSON.parse(
         await fsPromises.readFile(cartFilePath, { encoding: "utf-8" })
       );
 
       const newProduct = {
-        id: projectData.id,
-        name: projectData.name,
-        description: projectData.description,
-        price: projectData.price,
-		image: projectData.image,
+        id: productData.id,
+        name: productData.name,
+        description: productData.description,
+        price: productData.price,
+        image: productData.image,
       };
-
       cartData.products.push(newProduct);
-	  
-	  for (let i = 0; i < cartData.products.length; i++) {
-		
-		cartData.total += cartData.products[i].price
-
-	  }
-
-      // Parse the returned JSON project data into a JS object
-      //const cart = JSON.parse(cartData)
+      cartData.total = 0;
+      for (let i = 0; i < cartData.products.length; i++) {
+        cartData.total += cartData.products[i].price;
+      }
       await fsPromises.writeFile(cartFilePath, JSON.stringify(cartData));
-
       return cartData;
     },
-	removeFromCart: async (_, args) => {
-		const prodId = args.productId;
-		const cartId = args.cartId;
-		// Create a variable holding the file path (from computer root directory) to the project
-		// file we are looking for
-		const cartFilePath = path.join(cartDirectory, `${cartId}.json`);
-		const projectFilePath = path.join(projectDirectory, `${prodId}.json`);
-		const cartExists = await fileExists(cartFilePath);
-		if (!cartExists) return new GraphQLError("That cart does not exist");
-  
-		// Check if the requested project actually exists
-		const projectExists = await fileExists(projectFilePath);
-		// If project does not exist return an error notifying the user of this
-		if (!projectExists)
-		  return new GraphQLError("That project does not exist");
-  
-		// Read the project file; data will be returned as a JSON string
-		const projectData = JSON.parse(
-		  await fsPromises.readFile(projectFilePath, { encoding: "utf-8" })
-		);
-		let cartData = JSON.parse(
-		  await fsPromises.readFile(cartFilePath, { encoding: "utf-8" })
-		);
-  
-		for (let i = 0; i < cartData.products.length; i++) {
-			if (prodId === cartData.products[i].id) {
-				cartData.products.splice([i],1)
-				
-			}
-		}
-		
-		for (let i = 0; i < cartData.products.length; i++) {
-		  
-		  cartData.total += cartData.products[i].price
-  
-		}
-  
-		// Parse the returned JSON project data into a JS object
-		//const cart = JSON.parse(cartData)
-		await fsPromises.writeFile(cartFilePath, JSON.stringify(cartData));
-  
-		return cartData;
-	  },
+    removeFromCart: async (_, args) => {
+      const prodId = args.productId;
+      const cartId = args.cartId;
+      const cartFilePath = path.join(cartDirectory, `${cartId}.json`);
+      const productFilePath = path.join(productDirectory, `${prodId}.json`);
+      const cartExists = await fileExists(cartFilePath);
+      if (!cartExists) return new GraphQLError("That cart does not exist");
+      const productExists = await fileExists(productFilePath);
+      if (!productExists) {
+        return new GraphQLError("That product does not exist");
+      }
+      /* 		const productData = JSON.parse(
+		  await fsPromises.readFile(productFilePath, { encoding: "utf-8" })
+		); */
+      let cartData = JSON.parse(
+        await fsPromises.readFile(cartFilePath, { encoding: "utf-8" })
+      );
+      success = false;
+      for (let i = 0; i < cartData.products.length; i++) {
+        if (prodId === cartData.products[i].id && success === false) {
+          cartData.products.splice([i], 1);
+          success = true;
+        } 
+      }
+      cartData.total = 0;
+      for (let i = 0; i < cartData.products.length; i++) {
+        cartData.total += cartData.products[i].price;
+      }
+      await fsPromises.writeFile(cartFilePath, JSON.stringify(cartData));
+      if (success) {
+        return { deletedId: prodId, cartData, success };
+      }
+
+      return {success, deletedId: prodId}
+      
+    },
     deleteProduct: async (_, args) => {
-      // get project id
-      const projectId = args.projectId;
-
-      const filePath = path.join(projectDirectory, `${projectId}.json`);
-      // does this project exist?
-      // If no (return error)
-      const projectExists = await fileExists(filePath);
-      if (!projectExists)
+      const productId = args.productId;
+      const filePath = path.join(productDirectory, `${productId}.json`);
+      const productExists = await fileExists(filePath);
+      if (!productExists) {
         return new GraphQLError("That project does not exist");
-
-      // delete file
+      }
       try {
         await deleteFile(filePath);
       } catch (error) {
         return {
-          deletedId: projectId,
+          deletedId: productId,
           success: false,
         };
       }
-
       return {
-        deletedId: projectId,
+        deletedId: productId,
         success: true,
       };
     },
